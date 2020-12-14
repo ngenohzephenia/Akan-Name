067d74fd6d35660436fa0a6fc934b13033863db5
Moringa Access <ngenohzef@gmail.com>
2020-12-14 08:49:13 +0300

    added if ,else statement

--- css/script.js
+++ css/script.js
@@ -7,3 +7,16 @@ var mm = parseInt(document.getElementById("month").value);
 var dd = parseInt(document.getElementById("day").value);
 var gender = document.getElementById("gender").value;
 var d = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7
+if (mm <= 0 || mm > 12) {
+    alert("please enter a valid month")
+} else
+if (dd < 1 || dd > 31) {
+    alert("please enter a valid date")
+} else
+if (gender === "Male") {
+    alert("Your Ghanian akan Name is " + akanMaleNames[d] + " and you were born on " + dayOfTheWeek[d])
+} else
+ if (gender === "Female") {
+    alert("Your Ghanian akan Name is " + akanFemaleNames[d] + " and you were born on " + dayOfTheWeek[d])
+}
+}
