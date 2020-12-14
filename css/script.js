8c6b61f60dd572e2051557909577ae7ab620b099
Moringa Access <ngenohzef@gmail.com>
2020-12-14 09:26:02 +0300

    changes in README

--- css/script.js
+++ css/script.js
@@ -9,9 +9,8 @@ var gender = document.getElementById("gender").value;
 var d = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7
 if (mm <= 0 || mm > 12) {
     alert("please enter a valid month")
-} else
-if (dd < 1 || dd > 31) {
-    alert("please enter a valid date")
+} else if (dd < 1 || dd > 31) {
+    alert("please enter a valid date;1-31; 1-12");
 } else
 if (gender === "Male") {
     alert("Your Ghanian akan Name is " + akanMaleNames[d] + " and you were born on " + dayOfTheWeek[d])
