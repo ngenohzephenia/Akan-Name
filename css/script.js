136f9e1a9771c0b1afb3cddd902d0abc2bca926c
Moringa Access <ngenohzef@gmail.com>
2020-12-14 16:22:56 +0300

    script.js

--- css/script.js
+++ css/script.js
@@ -1,20 +1,27 @@
-var akanMaleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku ", "Yaw", "Kofi", "Kwame"];
-var akanFemaleNames = ["Akosua", "Adwoa", " Abenaa", "Akua", "  Yaa", "Afua","Ama"];
-var dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-var cc = parseInt(document.getElementById("century").value);
-var yy = parseInt(document.getElementById("year").value);
-var mm = parseInt(document.getElementById("month").value);
-var dd = parseInt(document.getElementById("day").value);
-var gender = document.getElementById("gender").value;
-var d = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7
-if (mm <= 0 || mm > 12) {
-    alert("please enter a valid month ;1-12");
-} else if (dd < 1 || dd > 31) {
-    alert("please enter a valid date;1-31");
-} else
-if (gender === "Male") {
-    alert("Your Ghanian akan Name is " + akanMaleNames[d] + " and you were born on " + dayOfTheWeek[d])
-} else
- if (gender === "Female") {
-    alert("Your Ghanian akan Name is " + akanFemaleNames[d] + " and you were born on " + dayOfTheWeek[d])
+var maleName = [" Kwasi", " Kwandwo", " Kwabena", " Kwaku", " Yaw", " Kofi", " Kwame"]
+var femaleName = [" Akosua", " Adwoa", " Abenaa", " Akua", " Yaa", " Afua", " Ama"]
+function getDay() {
+    var year = document.getElementById("year").value;
+    var mm = parseInt(document.getElementById("month").value);
+    var day = parseInt(document.getElementById("day").value);
+    var gender = document.getElementById("gender").value;
+    var yy = parseInt(year.substr(2, 4));
+    var century = parseInt(year.substr(0, 2));
+    var date = Math.abs(parseInt(((century / 4) - 2 * century - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + day) % 7)
+    if (year <= 0 || year == "" || year > 2020) {
+        alert("enter valid year")
+    
+    } else
+    if (mm <= 0 || mm > 12) {
+        alert("enter valid month")
+    } else
+    if (day < 1 || day > 31) {
+        alert("enter valid date")
+    }
+    if (gender === "male") {
+        alert("your Akan male name is" + maleName[date])
+    } else
+    if (gender === "female") {
+        alert("your Akan female name is"  + femaleName[date])
+    }
 }
