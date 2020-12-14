41a687a3209fad5ee0fe15df52f6ca7f2a194172
Moringa Access <ngenohzef@gmail.com>
2020-12-14 08:46:10 +0300

    added

--- css/script.js
+++ css/script.js
@@ -1,3 +1,9 @@
 var akanMaleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku ", "Yaw", "Kofi", "Kwame"];
 var akanFemaleNames = ["Akosua", "Adwoa", " Abenaa", "Akua", "  Yaa", "Afua","Ama"];
 var dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+var cc = parseInt(document.getElementById("century").value);
+var yy = parseInt(document.getElementById("year").value);
+var mm = parseInt(document.getElementById("month").value);
+var dd = parseInt(document.getElementById("day").value);
+var gender = document.getElementById("gender").value;
+var d = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7
