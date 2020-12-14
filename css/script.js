b3b3fcd943102d78542829e9daa79a3a9f762e20
Moringa Access <ngenohzef@gmail.com>
2020-12-14 08:41:35 +0300

    added variables

--- css/script.js
+++ css/script.js
@@ -1,1 +1,3 @@
 var akanMaleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku ", "Yaw", "Kofi", "Kwame"];
+var akanFemaleNames = ["Akosua", "Adwoa", " Abenaa", "Akua", "  Yaa", "Afua","Ama"];
+var dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
